362e67f9c1f7ac158ed2caf55cc7691810ce26b2
Lemon <AhoyLemon@users.noreply.github.com>
2018-09-01 10:42:59 -0500

    Initial commit

---
+++ js/pretend.js
@@ -0,0 +1,8 @@
+//@prepros-prepend partials/_impersonators.js
+//@prepros-prepend partials/_variables.js
+//@prepros-prepend partials/_functions.js
+
+//@prepros-prepend partials/_partymoods.js
+//@prepros-prepend partials/_cheese.js
+
+//@prepros-append partials/_vue.js
